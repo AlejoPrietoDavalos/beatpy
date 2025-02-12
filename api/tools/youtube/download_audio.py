6666c5e576348ba2adb1478358205b3a5c527ef4
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2025-02-12 02:24:29 -0300

    download_audio obtiene información de youtube para el path de salida

--- api/tools/youtube/download_audio.py
+++ api/tools/youtube/download_audio.py
@@ -1,16 +1,21 @@
+from pathlib import Path
+
 import yt_dlp
 
-def download_audio(url: str, output_path="./data"):
+
+def download_audio(url: str, path_out: Path) -> Path:
     opciones = {
         "format": "bestaudio/best",
-        "outtmpl": f"{output_path}/%(title)s.%(ext)s",
+        "outtmpl": str(path_out / "%(title)s.%(ext)s"),
         "postprocessors": [
             {"key": "FFmpegExtractAudio", "preferredcodec": "mp3", "preferredquality": "192"}
         ]
     }
 
     with yt_dlp.YoutubeDL(opciones) as ydl:
-        ydl.download([url])
-
+        info = ydl.extract_info(url, download=True)
+        title = info.get("title", "unknown")
+        path_audio = path_out / f"{title}.mp3"      # FIXME: Supone que es mp3. porque?
+    return path_audio
 #url_video = "https://www.youtube.com/watch?v=GVcOIlITUR8"
 #download_audio(url_video)
