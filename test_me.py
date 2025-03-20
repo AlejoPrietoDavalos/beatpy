94c65ac245e87372bddf555bac3d63a666b29da7
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2025-03-20 03:14:04 +0000

    .

--- test_me.py
+++ test_me.py
@@ -1,5 +1,9 @@
 import requests
-url_api = "http://127.0.0.1:8000/process_audio"
+from requests import Response
+
+def process_audio(*, url: str) -> Response:
+    url_api = "http://127.0.0.1:8000/process_audio"
+    return requests.post(url_api, json={"url": url})
 
 # Isolation
 #url = "https://www.youtube.com/watch?v=5ViMA_qDKTU"
@@ -7,8 +11,12 @@ url_api = "http://127.0.0.1:8000/process_audio"
 # Buddy Rich
 #url = "https://www.youtube.com/watch?v=aIjVro15Dns"
 
-url = "https://www.youtube.com/watch?v=ToQS7BzGMYE"
+# El cieguito volador
+#url = "https://www.youtube.com/watch?v=ynPjt2_Rb4I"
 
-r = requests.post(url_api, json={"url": url})
+# World in my eyes - Depeche Mode.
+url = "https://www.youtube.com/watch?v=KzqWe7uYo_A"
+
+r = process_audio(url=url)
 print(f"status_code={r.status_code}")
 print(r.text)
