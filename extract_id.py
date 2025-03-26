2ab952500d6787995fb1501b9dbe3088de52455b
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2025-03-26 02:13:58 +0000

    .

--- extract_id.py
+++
@@ -1,5 +0,0 @@
-
-
-print(extract_youtube_id(url="https://youtube.com/watch?v=5ViMA_qDKTU"))  # Salida: 5ViMA_qDKTU
-print(extract_youtube_id(url="http://youtube.com/watch?v=5ViMA_qDKTU"))   # Salida: 5ViMA_qDKTU
-print(extract_youtube_id(url="youtube.com/watch?v=5ViMA_qDKTU"))          # Salida: 5ViMA_qDKTU
