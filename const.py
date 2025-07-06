9fa306add0ca8447b6902d6a74ce130eb2ab4dd7
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2025-07-06 18:10:58 -0300

    Se mueve  plot_spectrograms_by_youtube a beatpy/plot.py

--- const.py
+++ const.py
@@ -5,3 +5,6 @@ path_data = Path(os.getenv("PATH_DATA"))    # TODO: En el servidor colocar bien.
 path_extracted = path_data / "extracted"
 path_data.mkdir(exist_ok=True)
 path_extracted.mkdir(exist_ok=True)
+
+COLOR_BACKGROUND = "#212167"
+COLOR_TEXT = "#9696f6"
