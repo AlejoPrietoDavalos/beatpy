e90ec7d2d1d660cee07651d8e23a29d8e233e80f
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2025-08-17 19:22:28 -0300

    ProcessAudioFormat.convert_to_mp3()

--- beatpy/process/audio/format.py
+++ beatpy/process/audio/format.py
@@ -7,7 +7,7 @@ DEFAULT_QUALITY = 0
 FFMPEG = "ffmpeg"
 
 
-class ConvertAudio:
+class ProcessAudioFormat:
     @staticmethod
     def _convert(*, path_in: Path, format_out: str, quality: T_Quality = DEFAULT_QUALITY, delete_old: bool = True) -> Path:
         if not path_in.exists():
@@ -20,5 +20,5 @@ class ConvertAudio:
         return path_out
 
     @staticmethod
-    def to_mp3(*, path_in: Path, quality: T_Quality = DEFAULT_QUALITY, delete_old: bool = True) -> Path:
-        ConvertAudio._convert(path_in=path_in, format_out="mp3", quality=quality, delete_old=delete_old)
+    def convert_to_mp3(*, path_in: Path, quality: T_Quality = DEFAULT_QUALITY, delete_old: bool = True) -> Path:
+        ProcessAudioFormat._convert(path_in=path_in, format_out="mp3", quality=quality, delete_old=delete_old)
