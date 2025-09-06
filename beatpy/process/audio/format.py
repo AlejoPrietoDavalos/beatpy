cb42b7754a3f92ef27df2bd6fabb957b77c8444b
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2025-09-06 15:14:03 -0300

    Convert @classmethod, format functions

--- beatpy/process/audio/format.py
+++ beatpy/process/audio/format.py
@@ -3,13 +3,15 @@ from pathlib import Path
 import subprocess
 
 T_Quality = Literal[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-DEFAULT_QUALITY = 0
+DEFAULT_QUALITY = 0     # La mejor.
 FFMPEG = "ffmpeg"
 
 
 class ProcessAudioFormat:
-    @staticmethod
-    def _convert(*, path_in: Path, format_out: str, quality: T_Quality = DEFAULT_QUALITY, delete_old: bool = True) -> Path:
+    FORMAT_MP3 = "mp3"
+
+    @classmethod
+    def _convert(cls, *, path_in: Path, format_out: str, quality: T_Quality = DEFAULT_QUALITY, delete_old: bool = True) -> Path:
         if not path_in.exists():
             raise ValueError(f"El archivo {path_in} no existe.")
         
@@ -19,6 +21,6 @@ class ProcessAudioFormat:
             path_in.unlink()
         return path_out
 
-    @staticmethod
-    def convert_to_mp3(*, path_in: Path, quality: T_Quality = DEFAULT_QUALITY, delete_old: bool = True) -> Path:
-        ProcessAudioFormat._convert(path_in=path_in, format_out="mp3", quality=quality, delete_old=delete_old)
+    @classmethod
+    def convert_to_mp3(cls, *, path_in: Path, quality: T_Quality = DEFAULT_QUALITY, delete_old: bool = True) -> Path:
+        ProcessAudioFormat._convert(path_in=path_in, format_out=cls.FORMAT_MP3, quality=quality, delete_old=delete_old)
