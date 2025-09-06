89a20ff0be13208e8d4a706574782747fb4fc222
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2025-09-06 15:16:02 -0300

    Move format inside beatpy.audio

--- beatpy/audio/format.py
+++ beatpy/audio/format.py
@@ -7,7 +7,7 @@ DEFAULT_QUALITY = 0     # La mejor.
 FFMPEG = "ffmpeg"
 
 
-class ProcessAudioFormat:
+class AudioFormat:
     FORMAT_MP3 = "mp3"
 
     @classmethod
@@ -23,4 +23,4 @@ class ProcessAudioFormat:
 
     @classmethod
     def convert_to_mp3(cls, *, path_in: Path, quality: T_Quality = DEFAULT_QUALITY, delete_old: bool = True) -> Path:
-        ProcessAudioFormat._convert(path_in=path_in, format_out=cls.FORMAT_MP3, quality=quality, delete_old=delete_old)
+        AudioFormat._convert(path_in=path_in, format_out=cls.FORMAT_MP3, quality=quality, delete_old=delete_old)
