6aefa0b87f59b6f64c5f4810356c03a3f62adfda
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2025-09-10 19:24:07 -0300

    Create DrumMachineScene with pattern

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
-import json
-
-from drum_machine.entities.pattern import DrumPattern
+from drum_machine.entities.pattern import DrumPatterns
 from drum_machine.player import DrumMachinePlayer
 from pgstudio.scene import SceneBase
 from pgstudio.client import init_pygame, ClientPGS, ConfigClientPGS
-
-
-def get_pattern(path: str) -> DrumPattern:
-    with open(path, "r") as f:
-        data = json.load(f)
-    return DrumPattern(**data)
+from patterns.porcupine_tree import PorcupineTree
 
 
 class DrumMachineScene(SceneBase):
-    def __init__(self, *, name: str):
+    def __init__(self, *, name: str, drum_patterns: DrumPatterns):
         super().__init__(name=name)
-        self.pattern = get_pattern("porcupine_tree_the_sound_of_muzak.json")
-        self.player = DrumMachinePlayer(pattern=self.pattern)
+        self.player = DrumMachinePlayer(drum_patterns=drum_patterns)
 
     def main(self) -> None:
         self.player.update()
@@ -24,7 +16,10 @@ class DrumMachineScene(SceneBase):
 
 def get_scenes():
     return {
-        "drum_machine": DrumMachineScene(name="Drum Machine")
+        "drum_machine": DrumMachineScene(
+            name="Drum Machine",
+            drum_patterns=PorcupineTree.the_sound_of_the_muzak()
+        )
     }
 
 
