8ee99ec620970f963b61235c41a38535c85551e7
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2025-09-10 22:15:07 -0300

    Muevo fuera la escena, levanto el .env y configuro logs

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-from drum_machine.entities.pattern import DrumPatterns
-from drum_machine.player import DrumMachinePlayer
-from pgstudio.scene import SceneBase
+from dotenv import load_dotenv
 from pgstudio.client import init_pygame, ClientPGS, ConfigClientPGS
 from patterns.porcupine_tree import PorcupineTree
+from scenes.drum_machine import DrumMachineScene
+from configure_logging import configure_logging
 
-
-class DrumMachineScene(SceneBase):
-    def __init__(self, *, name: str, drum_patterns: DrumPatterns):
-        super().__init__(name=name)
-        self.player = DrumMachinePlayer(drum_patterns=drum_patterns)
-
-    def main(self) -> None:
-        self.player.update()
+load_dotenv()
+configure_logging()
 
 
 def get_scenes():
