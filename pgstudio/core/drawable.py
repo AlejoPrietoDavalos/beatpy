1dda2cf51aa67ce8dcd61bf6c82255739bdb9f09
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2025-09-20 08:38:43 -0300

    Add Drawabke.set_position, and rename Rectangle -> Rect

--- pgstudio/core/drawable.py
+++ pgstudio/core/drawable.py
@@ -25,11 +25,17 @@ class PyGameDraw:
 
 class Drawable(ABC):
     @abstractmethod
+    def set_position(self, x: int, y: int) -> None:
+        """Mueve el objeto a la posición (x, y)."""
+        ...
+
+    @abstractmethod
     def draw(self, surface: pg.Surface) -> None:
+        """Dibuja el objeto en la `surface`."""
         ...
 
 
-class Rectangle(Drawable):
+class Rect(Drawable):
     def __init__(self, *, color: T_Color, x: int, y: int, width: int, height: int):
         self.color = color
         self.rect = pg.Rect(x, y, width, height)
@@ -40,12 +46,17 @@ class Rectangle(Drawable):
     def draw(self, surface: pg.Surface) -> None:
         PyGameDraw.rect(surface, self.color, self.rect)
 
+
 class Circle(Drawable):
     def __init__(self, *, color: T_Color, x: int, y: int, radius: int):
         self.color = color
         self.x = x
         self.y = y
         self.radius = radius
+
+    def set_position(self, *, x: int, y: int) -> None:
+        self.x = x
+        self.y = y
 
     def draw(self, surface: pg.Surface) -> None:
         PyGameDraw.circle(surface=surface, color=self.color, x=self.x, y=self.y, radius=self.radius)
